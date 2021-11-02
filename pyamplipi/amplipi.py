81f489c20e38cc632aeadd0d319ba82f3c90bcd1
brian <non@non>
2021-11-01 19:34:32 -0500

    Make the amplipi wrapper await

--- pyamplipi/amplipi.py
+++ pyamplipi/amplipi.py
@@ -25,78 +25,78 @@ class AmpliPi:
             disable_insecure_warning,
         )
 
-    def get_status(self) -> Status:
+    async def get_status(self) -> Status:
         response = await self._client.get('')
         return Status.parse_obj(response)
 
-    def get_sources(self) -> List[Source]:
+    async def get_sources(self) -> List[Source]:
         response = await self._client.get('sources')
         return [Source.parse_obj(source) for source in response['sources']]
 
-    def get_source(self, source_id: int) -> Source:
+    async def get_source(self, source_id: int) -> Source:
         response = await self._client.get(f'sources/{source_id}')
         return Source.parse_obj(response)
 
-    def set_source(self, source_id: int, source_update: SourceUpdate) -> Source:
+    async def set_source(self, source_id: int, source_update: SourceUpdate) -> Source:
         response = await self._client.patch(f'sources/{source_id}', json.dumps(source_update))
         return Source.parse_obj(response)
 
-    def get_zones(self) -> List[Zone]:
+    async def get_zones(self) -> List[Zone]:
         response = await self._client.get('zones')
         return [Zone.parse_obj(zone) for zone in response]
 
-    def set_zones(self, zone_update: MultiZoneUpdate) -> Status:
+    async def set_zones(self, zone_update: MultiZoneUpdate) -> Status:
         response = await self._client.patch('zones', json.dumps(zone_update))
         return Status.parse_obj(response)
 
-    def set_zone(self, zone_id: int, zone_update: ZoneUpdate) -> Zone:
+    async def set_zone(self, zone_id: int, zone_update: ZoneUpdate) -> Zone:
         response = await self._client.patch(f'zones/{zone_id}', json.dumps(zone_update))
         return Zone.parse_obj(response)
 
-    def create_group(self, new_group: Group) -> Group:
+    async def create_group(self, new_group: Group) -> Group:
         response = await self._client.post('group', json.dumps(new_group))
         return Group.parse_obj(response)
 
-    def get_groups(self) -> List[Group]:
+    async def get_groups(self) -> List[Group]:
         response = await self._client.get('groups')
         return [Group.parse_obj(group) for group in response]
 
-    def get_group(self, group_id) -> Group:
+    async def get_group(self, group_id) -> Group:
         response = await self._client.get(f'groups/{group_id}')
         return Group.parse_obj(response)
 
-    def delete_group(self, group_id):
+    async def delete_group(self, group_id):
         response = await self._client.delete(f'groups/{group_id}')
         return Status.parse_obj(response)
 
-    def set_group(self, group_id, update: GroupUpdate) -> Group:
+    async def set_group(self, group_id, update: GroupUpdate) -> Group:
         response = await self._client.patch(f'groups/{group_id}', update.json())
         return Group.parse_obj(response)
 
-    def get_streams(self) -> Stream:
+    async def get_streams(self) -> Stream:
         response = await self._client.get('streams')
         return Stream.parse_obj(response)
 
-    def get_stream(self, stream_id: int) -> Stream:
+    async def get_stream(self, stream_id: int) -> Stream:
         response = await self._client.get(f'streams/{stream_id}')
         return Stream.parse_obj(response)
 
-    def create_stream(self, new_stream: Stream) -> Stream:
+    async def create_stream(self, new_stream: Stream) -> Stream:
         response = await self._client.post(f'streams', json.dumps(new_stream))
         return Stream.parse_obj(response)
 
-    def delete_stream(self, stream_id: int) -> Status:
+    async def delete_stream(self, stream_id: int) -> Status:
         response = await self._client.delete(f'streams/{stream_id}')
         return Status.parse_obj(response)
 
-    def set_stream(self, stream_id: int, update: StreamUpdate) -> Stream:
+    async def set_stream(self, stream_id: int, update: StreamUpdate) -> Stream:
         response = await self._client.post(f'streams/{stream_id}', json.dumps(update))
         return Stream.parse_obj(response)
 
-    def announce(self, announcement: Announcement) -> Status:
+    async def announce(self, announcement: Announcement) -> Status:
         response = await self._client.post('announce', json.dumps(announcement))
         return Status.parse_obj(response)
 
-    def get_presets(self) -> List[Preset]:
+    async def get_presets(self) -> List[Preset]:
         response = await self._client.get('presets')
         return [Preset.parse_obj(preset) for preset in response]
